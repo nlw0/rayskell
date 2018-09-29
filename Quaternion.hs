14728d030ae1954ddd770aedd2937099c03bbd3e
nic <nwerneck@gmail.com>
2018-09-29 16:35:10 +0200

    vec fro pixel

--- Quaternion.hs
+++ Quaternion.hs
@@ -1,3 +1,5 @@
+module Quaternion where
+
 qrot [a, b, c, d] = [
    [(a * a + b * b - c * c - d * d), (2 * b * c - 2 * a * d), (2 * b * d + 2 * a * c)],
    [(2 * b * c + 2 * a * d), (a * a - b * b + c * c - d * d), (2 * c * d - 2 * a * b)],
--- genpic.hs
+++ genpic.hs
@@ -1,14 +1,39 @@
-
-
-pix = [[123,432,2],[354,345,532],[345,34,34]]
+import Quaternion
 
 iw :: Integer
-iw = 1024
+iw = 10 --24
 ih :: Integer
-ih = 798
+ih = 7 --98
+oc :: [Float]
 oc = [(0.5 + ((fromIntegral iw) / (-2.0))), (0.5 + ((fromIntegral ih) / (-2.0))), ((fromIntegral iw) / 2.0)]
+tilt:: Float
 tilt = 0.6
 
+sky_color = [63, 126, 226]
+plane_white = [255, 255, 255]
+plane_black = [0, 0, 0]
+error_color = [255, 0, 0]
+
+oo1 = [0, -1.5, -0.6]
+oo2 = [1.2, -1.5, -0.6]
+oo3 = [-1.2, -1.5, -0.6]
+rr = 0.6
+
+fmypix = do j <- [0..(ih - 1)]
+            k <- [0..(iw - 1)]
+            find_ray_color $ vec_from_pixel [j, k]
+
+vec_from_pixel :: [Integer] -> [Float]
+vec_from_pixel [j, k] = -- [1, 0, 0]
+  let cw = [0.0, 0.0, -2.0]
+      qq = [(1.0 - (tilt * tilt)) , (tilt * tilt), 0.0, 0.0]
+      vv = rotate qq $ vsum [(fromInteger k), (fromInteger j), 0.0] oc
+      in vv
+
+find_ray_color _ = sky_color
+
+
+pix = [[1,2],[3,4]]
 p6_header = ["P3", ((show iw) ++ " " ++ (show ih)), "255"]
 
 file_contents = unlines $ p6_header ++ [unwords $ map show $ concat pix]
